28cbca597b485b4bcdf01bb986d20e48aa65b389
Gerald <geraldwainaina25@gmail.com>
2023-04-20 07:58:03 +0300

    Update savingandloadingmodels.py

--- savingandloadingmodels.py
+++ savingandloadingmodels.py
@@ -1,9 +1,9 @@
-#We take a trained model, save it, then load it back.
-#We will save it as a HDF5 file, which is the format used by Keras
-#concepts
-'''
+# We take a trained model, save it, then load it back.
+# We will save it as a HDF5 file, which is the format used by Keras
+# concepts
+"""
         saving models in HDF5 format
         saving models in Tensorflow SavedModel format
         loading models
         download models to Local Disk for deployment to different platforms
-'''
+"""
